c4e4b3321aa84af036b782e4e18cc619a71fc2be
One2r <601941036@qq.com>
2017-08-03 10:04:53 +0800

    样式调整

--- src/donate-config.js
+++ src/donate-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	wxpay:'',
+	wxpay : false,
+	wxpay_qrcode : require("../css/wxpay.png"),
+	alipay : false,
+	alipay_qrcode : require("../css/alipay.png"),
 	donateUI :'<div class="article-reward">\
                 	<div class="article-reward-desc">「真诚赞赏，手留余香」</div>\
                 	<button class="reward-btn" id="reward-btn">￥ 赞&nbsp;赏</button>\
                 	<div class="article-reward-desc">您的支持将鼓励我继续创作 :)</div>\
-				</div>',
-
-	wxDonate : '<img class="reward-qrcode" src="'+require("../css/wxpay.png")+'">\
-				<div>微信扫一扫</div>',
+				</div>'
 }
--- src/donate-plugin.js
+++ src/donate-plugin.js
@@ -11,19 +11,30 @@ $.extend({
 		if (id == undefined){
 			return;
 		}
+		console.log(options)
+		var payObj = [];
+		if(options.wxpay){
+			payObj.push({
+					title: '微信赞赏',
+					content: '<div style="text-align: center; width: 90%;"><img class="reward-qrcode" src="'+options.wxpay_qrcode+'"/><div>微信扫一扫</div><div>'
+			})
+		}
+		if(options.alipay){
+			payObj.push({
+					title: '支付宝赞赏',
+					content: '<div style="text-align: center; width: 90%;"><img class="reward-qrcode" src="'+options.alipay_qrcode+'"/><div>支付宝扫一扫</div><div>'
+			})
+		}
+		if(payObj.length == 0 ){
+			return;
+		}
 		$(id).html(options.donateUI);
 		$(id+" .reward-btn").on('click', function () {
 			layer.tab({
 				type: 1,
 				shadeClose: true,
 				area: ['350px', '270px'],
-				tab: [{
-					title: '微信赞赏',
-					content: options.wxDonate
-				}, {
-					title: '支付宝赞赏',
-					content: '内容2'
-				}]
+				tab: payObj
 			});
 		})
     }
